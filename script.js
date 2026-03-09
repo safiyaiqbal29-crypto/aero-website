8a857c6f81b15b3d4f369b79fb10b1f0b88d1cc0
safiyaiqbal29-crypto <safiya.iqbal29@gmail.com>
2026-03-09 19:41:10 +0000

    Add lift simulator and quiz functionality

---
+++ script.js
@@ -0,0 +1,91 @@
+// Lift simulator
+
+let slider = document.getElementById("speedSlider");
+
+if(slider){
+
+slider.oninput = function(){
+
+let lift = this.value * 2;
+
+document.getElementById("liftOutput").innerText =
+"Estimated Lift Force: " + lift + " units";
+
+}
+
+}
+
+// Quiz
+
+function answer(choice){
+
+if(choice === "lift"){
+
+document.getElementById("quizResult").innerText =
+"Correct! Lift keeps aircraft in the air.";
+
+}
+
+else{
+
+document.getElementById("quizResult").innerText =
+"Not quite. Try again.";
+
+}
+
+}
+
+// Scroll animations
+
+let sections = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+
+sections.forEach(sec => {
+
+let top = sec.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+
+sec.classList.add("show");
+
+}
+
+});
+
+});
+function answer(choice){
+
+if(choice === "lift"){
+document.getElementById("quizResult").innerText = "Correct! Lift keeps aircraft in the air.";
+}
+
+else{
+document.getElementById("quizResult").innerText = "Not quite. Try again.";
+}
+
+}
+
+function answer2(choice){
+
+if(choice === "delta"){
+document.getElementById("quizResult").innerText = "Correct! Delta wings are used for supersonic flight.";
+}
+
+else{
+document.getElementById("quizResult").innerText = "Incorrect. Try again.";
+}
+
+}
+
+function answer3(choice){
+
+if(choice === "weight"){
+document.getElementById("quizResult").innerText = "Correct! Weight opposes lift.";
+}
+
+else{
+document.getElementById("quizResult").innerText = "Incorrect. Try again.";
+}
+
+}
